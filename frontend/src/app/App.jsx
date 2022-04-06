a1e08279bf1637f468352f0ace9ed413f27b8c7a
Charles-Abasimfon <devabascharles@gmail.com>
2022-04-06 14:31:10 +0100

    Improved loader

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //Custom Components
@@ -15,122 +15,139 @@ import CaseStudyDetails from '../pages/case-study/case-study-details';
 import Contact from '../pages/contact';
 import Error from '../pages/404';
 import AutoScrollToTop from '../components/ScrollTop/AutoScrollToTop';
+import PageLoading from '../components/Loader/PageLoading';
 
 const App = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    let timer = setTimeout(() => setLoaded(true), 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route
-            path='/'
-            exact
-            element={
-              <AutoScrollToTop>
-                <Home />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/home'
-            element={
-              <AutoScrollToTop>
-                <Home />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/home-2'
-            element={
-              <AutoScrollToTop>
-                <HomeTwo />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/home-3'
-            element={
-              <AutoScrollToTop>
-                <HomeThree />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/about'
-            element={
-              <AutoScrollToTop>
-                <About />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/blog'
-            exact
-            element={
-              <AutoScrollToTop>
-                <Blog />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/blog/blog-details'
-            element={
-              <AutoScrollToTop>
-                <BlogDetails />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/services'
-            exact
-            element={
-              <AutoScrollToTop>
-                <Service />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/service/service-details'
-            element={
-              <AutoScrollToTop>
-                <ServiceDetails />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/case-study'
-            exact
-            element={
-              <AutoScrollToTop>
-                <CaseStudy />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/case-study/case-study-details'
-            element={
-              <AutoScrollToTop>
-                <CaseStudyDetails />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            path='/contact'
-            element={
-              <AutoScrollToTop>
-                <Contact />
-              </AutoScrollToTop>
-            }
-          />
-          <Route
-            element={
-              <AutoScrollToTop>
-                <Error />
-              </AutoScrollToTop>
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      {/* using a ternary operator to determine which component to display
+    based on the loaded state. If loaded is false the Loader is rendered */}
+      {!loaded ? (
+        <PageLoading />
+      ) : (
+        <div className='App'>
+          <Router>
+            <Routes>
+              <Route
+                path='/'
+                exact
+                element={
+                  <AutoScrollToTop>
+                    <Home />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/home'
+                element={
+                  <AutoScrollToTop>
+                    <Home />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/home-2'
+                element={
+                  <AutoScrollToTop>
+                    <HomeTwo />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/home-3'
+                element={
+                  <AutoScrollToTop>
+                    <HomeThree />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/about'
+                element={
+                  <AutoScrollToTop>
+                    <About />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/blog'
+                exact
+                element={
+                  <AutoScrollToTop>
+                    <Blog />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/blog/blog-details'
+                element={
+                  <AutoScrollToTop>
+                    <BlogDetails />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/services'
+                exact
+                element={
+                  <AutoScrollToTop>
+                    <Service />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/service/service-details'
+                element={
+                  <AutoScrollToTop>
+                    <ServiceDetails />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/case-study'
+                exact
+                element={
+                  <AutoScrollToTop>
+                    <CaseStudy />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/case-study/case-study-details'
+                element={
+                  <AutoScrollToTop>
+                    <CaseStudyDetails />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                path='/contact'
+                element={
+                  <AutoScrollToTop>
+                    <Contact />
+                  </AutoScrollToTop>
+                }
+              />
+              <Route
+                element={
+                  <AutoScrollToTop>
+                    <Error />
+                  </AutoScrollToTop>
+                }
+              />
+            </Routes>
+          </Router>
+        </div>
+      )}
+    </>
   );
 };
 
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -3,30 +3,13 @@ import HomeMain from './HomeMain';
 import HeaderStyleTwo from '../../components/Header/HeaderStyleTwo';
 import Footer from '../../components/Footer';
 import footerLogo from '../../assets/images/logos/logo-white.png';
-import PageLoading from '../../components/Loader/PageLoading';
 
 function HomePage() {
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    let timer = setTimeout(() => setLoaded(true), 1000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
   return (
     <React.Fragment>
-      {/* using a ternary operator to determine which component to display
-    based on the loaded state. If loaded is false the Loader is rendered */}
-      {!loaded ? (
-        <PageLoading />
-      ) : (
-        <>
-          <HeaderStyleTwo parentMenu='home' />
-          <HomeMain />
-          <Footer footerLogo={footerLogo} footerCTA='enable' />
-        </>
-      )}
+      <HeaderStyleTwo parentMenu='home' />
+      <HomeMain />
+      <Footer footerLogo={footerLogo} footerCTA='enable' />
     </React.Fragment>
   );
 }
