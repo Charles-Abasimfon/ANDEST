269f8740b7b9dfb802120d39254b733d2671ca56
Charles-Abasimfon <devabascharles@gmail.com>
2022-04-06 09:15:31 +0100

    First attempt to fix responsiveness on mobile

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -6,8 +6,6 @@ import Footer from '../../components/Footer';
 import footerLogo from '../../assets/images/logos/logo-white.png';
 
 class HomePage extends React.Component {
-  componentDidMount() {}
-
   render() {
     return (
       <React.Fragment>
